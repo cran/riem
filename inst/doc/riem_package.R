3eb22256727946d2ddac14d186317912c7408a13
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-09-10 01:40:34 +0000

    version 0.1.1

--- inst/doc/riem_package.R
+++ inst/doc/riem_package.R
@@ -1,3 +1,15 @@
+## ---- echo = FALSE-------------------------------------------------------
+NOT_CRAN <- identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  purl = NOT_CRAN,
+  eval = NOT_CRAN
+)
+
+## ----eval = FALSE--------------------------------------------------------
+#  install.packages("riem")
+
 ## ---- eval = FALSE-------------------------------------------------------
 #  library("devtools")
 #  install_github("ropenscilabs/riem")
@@ -5,12 +17,12 @@
 
 ## ---- warning = FALSE, message = FALSE-----------------------------------
 library("riem")
-library("dplyr")
-riem_networks() %>% head() %>% knitr::kable()
+riem_networks() 
 
 ## ------------------------------------------------------------------------
-riem_stations(network = "IN__ASOS") %>% head() %>% knitr::kable()
+riem_stations(network = "IN__ASOS") 
 
 ## ------------------------------------------------------------------------
-riem_measures(station = "VOHY", date_start = "2000-01-01", date_end = "2016-04-22") %>% head() %>% knitr::kable()
+measures <- riem_measures(station = "VOHY", date_start = "2000-01-01", date_end = "2016-04-22") 
+head(measures)
 
